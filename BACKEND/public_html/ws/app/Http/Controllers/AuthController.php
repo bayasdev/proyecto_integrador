20bffff0215dbd6ae04d91551d6f11fb5502d0f2
Victor Bayas <victorsbayas@gmail.com>
2022-01-20 08:54:07 -0500

    build

--- BACKEND/public_html/ws/app/Http/Controllers/AuthController.php
+++ BACKEND/public_html/ws/app/Http/Controllers/AuthController.php
@@ -59,7 +59,7 @@ class AuthController extends Controller
     $subject = 'Recuperación de Contraseña';
     $user = User::where('id', $credentials->subject)->first();
     $resp = $this->send_mail('mail', $user->email, $user->name, $subject, $message, env('MAIL_FROM_ADDRESS'), env('MAIL_FROM_NAME'));
-    return redirect()->away(env('FRONT_URL'));
+    return redirect(env('FRONT_URL'));
   }
 
   function passwordChange(Request $data)

82169623f3e4e767a2ce03954b0dc76d848e92d8
pasthortown <luissalazarvaca1986@gmail.com>
2022-01-20 09:11:05 -0500

    build

--- FRONTEND/client/src/app/layout/attended-request-page/attended-request-page.component.ts
+++ FRONTEND/client/src/app/layout/attended-request-page/attended-request-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-attended-request-page',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AttendedRequestPageComponent implements OnInit {
 
-  constructor() { }
+  title: string = '';
+  message: string = '';
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe((params: any) => {
+      this.title = params.title;
+      this.message = params.message;
+    });
   }
 
 }

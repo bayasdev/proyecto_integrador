a704a4c4245b0a6fa08eb92e326809e692f188ad
Victor Bayas <victorsbayas@gmail.com>
2022-02-07 09:05:13 -0500

    Revert

--- frontend/client/deploy.bat
+++
@@ -1,6 +0,0 @@
-cd ..\..\server\var\www\html
-rmdir frontend /Q/S
-mkdir frontend
-cd ..\..\..\..\frontend\client
-ng build --base-href "https://smcar.bayas.dev/"
-xcopy dist\client ..\..\server\var\www\html\frontend\
